eea891f9f61bd2eed049def8c3475bf724ac6ec6
chenaoh <cdhenao@gmail.com>
2022-10-02 20:30:15 -0400

    visual code

---
+++ principalVSC.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout <<"Mi nombre es Cesar Soria Mita naci el 25 de de febrero del 2003 en el mucipio de Mojocoya-Zudanies, desde que naci me crie en el chaco chuquisaquenio actualmente tengo 19 anios y estoy estudiando la carrrera de ING de Sistemas" << endl; 
+}
